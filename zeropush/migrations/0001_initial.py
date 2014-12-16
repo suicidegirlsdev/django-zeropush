931ee31287d1aaf76f764c4cd4f6f9f4557fb8b5
Dimitri Gnidash <dimitri.gnidash@gmail.com>
2014-12-16 12:37:24 -0500

    -Add sgauth model to the frozen tables

--- zeropush/migrations/0001_initial.py
+++ zeropush/migrations/0001_initial.py
@@ -9,7 +9,7 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'InstagramProfile'
-        db.create_table(u'zeropush_pushdevice', (
+        db.create_table(u'pushdevice', (
             ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sgauth.User'])),
             ('token', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
         ))
@@ -17,6 +17,29 @@ class Migration(SchemaMigration):
 
     def backwards(self, orm):
         # Deleting model 'InstagramProfile'
-        db.delete_table(u'zeropush_pushdevice')
+        db.delete_table(u'pushdevice')
 
+    models {
+        u'sgauth.user': {
+            'Meta': {'ordering': "['username']", 'object_name': 'User'},
+            'anonymous': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '254', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'followees': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'followers'", 'symmetrical': 'False', 'through': u"orm['following.Follow']", 'to': u"orm['sgauth.User']"}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'legacy_id': ('django.db.models.fields.IntegerField', [], {'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'legacy_model_password': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'legacy_password': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '40', 'db_index': 'True'})
+        }
+    }
     complete_apps = ['zeropush']
